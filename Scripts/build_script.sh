28a3e742b0e2374e32068ced238ec026ccc12767
hcl <gtech.hcl@gmail.com>
2014-04-14 14:44:49 +0530

    remove
    changed

--- Scripts/build_script.sh
+++ Scripts/build_script.sh
@@ -9,11 +9,6 @@ function deployToTestFlight
 {
 curl http://testflightapp.com/api/builds.json -F file=@$build_location/$appname.ipa -F dsym=@$build_location/sym.root/Release-iphoneos/$appname.app.dSYM.zip -F api_token='439d831c7e49dbda0210fcc2821ac279_MTc2MzA5NTIwMTQtMDQtMDIgMDk6MjY6MzIuODYyNTg1' -F team_token='4eec8f2464f97aa31e333520338d7197_MzYyMjkzMjAxNC0wNC0wMiAwOTo0Mjo0Mi4xMzcxMTQ' -F notes='$appname uploaded via the testflight upload API' -F notify=True
 }
-function testflight
-{
-git remote add origin https://github.com/gtech123/sampleproject.git
-git push -u origin master
-}
 function buildApp 
 
 {
@@ -35,8 +30,6 @@ zip -r "$appname.app.dSYM.zip" "$appname.app.dSYM"
   
 xcrun -sdk iphoneos PackageApplication -v "$build_location/sym.root/Release-iphoneos/$appname.app" -o "$build_location/$appname.ipa" --sign "$certificate"   
 deployToTestFlight
-testflight
-
 }
 
 
