737b8c7fce576eff79824f4ec056bbe8ec08b92a
hcl <gtech.hcl@gmail.com>
2014-04-14 18:27:51 +0530

    fix

--- Scripts/build_script.sh
+++ Scripts/build_script.sh
@@ -3,11 +3,11 @@ target_name="$appname"
 sdk="iphoneos"
 certificate="iPhone Distribution: HCL Technologies Ltd"
 project_dir="$1/$appname"
-build_location="/Users/admin/Documents/Builds/sampleproject"
+build_location="/Users/admin/Documents/Builds/$appname"
 
 function deployToTestFlight    
 {
-curl http://testflightapp.com/api/builds.json -F file=@$build_location/sample.ipa -F dsym=@$build_location/sym.root/Release-iphoneos/sample.app.dSYM.zip -F api_token='439d831c7e49dbda0210fcc2821ac279_MTc2MzA5NTIwMTQtMDQtMDIgMDk6MjY6MzIuODYyNTg1' -F team_token='4eec8f2464f97aa31e333520338d7197_MzYyMjkzMjAxNC0wNC0wMiAwOTo0Mjo0Mi4xMzcxMTQ' -F notes='$appname uploaded via the testflight upload API' -F notify=True
+curl http://testflightapp.com/api/builds.json -F file=@$build_location/$appname.ipa -F dsym=@$build_location/sym.root/Release-iphoneos/$appname.app.dSYM.zip -F api_token='439d831c7e49dbda0210fcc2821ac279_MTc2MzA5NTIwMTQtMDQtMDIgMDk6MjY6MzIuODYyNTg1' -F team_token='4eec8f2464f97aa31e333520338d7197_MzYyMjkzMjAxNC0wNC0wMiAwOTo0Mjo0Mi4xMzcxMTQ' -F notes='$appname uploaded via the testflight upload API' -F notify=True
 }
 function buildApp 
 {
@@ -26,10 +26,10 @@ xcodebuild -target "sampleproject" OBJROOT="$build_location/obj.root" SYMROOT="$
 
 #zip dYSM file for distribution
 cd "$build_location/sym.root/Release-iphoneos/" || die "no such directory"
-rm -f "sample.app.dSYM.zip"
-zip -r "sample.app.dSYM.zip" "sample.app.dSYM"
+rm -f "$appname.app.dSYM.zip"
+zip -r "$appname.app.dSYM.zip" "sample.app.dSYM"
   
-xcrun -sdk iphoneos PackageApplication -v "$build_location/sym.root/Release-iphoneos/sample.app" -o "$build_location/sample.ipa" --sign "$certificate"   
+xcrun -sdk iphoneos PackageApplication -v "$build_location/sym.root/Release-iphoneos/$appname.app" -o "$build_location/$appname.ipa" --sign "$certificate"   
 
 deployToTestFlight
 
