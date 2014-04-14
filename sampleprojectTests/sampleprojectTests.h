cd0b1b446a9afdeff63ad0765b7b065f1c555e74
hcl <gtech.hcl@gmail.com>
2014-04-14 12:01:21 +0530

    Revert "tryy"
    This reverts commit f61809d0fe7729f28e41a7dcb351ef9c3f333beb.

---
+++ sampleprojectTests/sampleprojectTests.h
@@ -0,0 +1,13 @@
+//
+//  sampleprojectTests.h
+//  sampleprojectTests
+//
+//  Created by admin on 20/03/14.
+//  Copyright (c) 2014 admin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface sampleprojectTests : SenTestCase
+
+@end
