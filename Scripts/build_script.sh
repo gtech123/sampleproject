8e0c59cbe72fb47640df7b6c5eac61229d93b91e
hcl <gtech.hcl@gmail.com>
2014-04-14 17:21:58 +0530

    $appname

--- Scripts/build_script.sh
+++ Scripts/build_script.sh
@@ -7,7 +7,7 @@ build_location="/Users/admin/Documents/Builds/$appname"
 
 function deployToTestFlight 
 {
-curl http://testflightapp.com/api/builds.json -F file=@$build_location/$appname.ipa -F dsym=@$build_location/sym.root/Release-iphoneos/sampleproject.app.dSYM.zip -F api_token='439d831c7e49dbda0210fcc2821ac279_MTc2MzA5NTIwMTQtMDQtMDIgMDk6MjY6MzIuODYyNTg1' -F team_token='4eec8f2464f97aa31e333520338d7197_MzYyMjkzMjAxNC0wNC0wMiAwOTo0Mjo0Mi4xMzcxMTQ' -F notes='$appname uploaded via the testflight upload API' -F notify=True
+curl http://testflightapp.com/api/builds.json -F file=@$build_location/$appname.ipa -F dsym=@$build_location/sym.root/Release-iphoneos/$appname.app.dSYM.zip -F api_token='439d831c7e49dbda0210fcc2821ac279_MTc2MzA5NTIwMTQtMDQtMDIgMDk6MjY6MzIuODYyNTg1' -F team_token='4eec8f2464f97aa31e333520338d7197_MzYyMjkzMjAxNC0wNC0wMiAwOTo0Mjo0Mi4xMzcxMTQ' -F notes='$appname uploaded via the testflight upload API' -F notify=True
 }
 function buildApp 
 {
@@ -20,16 +20,16 @@ mkdir -p "$build_location"/
 fi
 
 
-cd /Users/admin/Documents/sampleproject/
-xcodebuild -target "sampleproject" OBJROOT="$build_location/obj.root" SYMROOT="$build_location/sym.root"
+cd /Users/admin/Documents/$appname/
+xcodebuild -target "$appname" OBJROOT="$build_location/obj.root" SYMROOT="$build_location/sym.root"
 
 #zip dYSM file for distribution 
 cd "$build_location/sym.root/Release-iphoneos/" || die "no such directory"
-rm -f "sampleproject.app.dSYM.zip"
-zip -r "sampleproject.app.dSYM.zip" "sampleproject.app.dSYM"
-zip -r sampleproject.app.dSYM.zip . -i sampleproject.app.dSYM
+rm -f "$appname.app.dSYM.zip"
+zip -r "$appname.app.dSYM.zip" "$appname.app.dSYM"
+zip -r $appname.app.dSYM.zip . -i $appname.app.dSYM
   
-xcrun -sdk iphoneos PackageApplication -v "$build_location/sym.root/Release-iphoneos/sampleproject.app" -o "$build_location/sampleproject.ipa" --sign "$certificate"   
+xcrun -sdk iphoneos PackageApplication -v "$build_location/sym.root/Release-iphoneos/$appname.app" -o "$build_location/$appname.ipa" --sign "$certificate"   
 
 deployToTestFlight
 
