bd52748be4ee34337135ffd9acf3d01afe2fdcef
hcl <gtech.hcl@gmail.com>
2014-04-14 15:06:57 +0530

    change
    format

--- Scripts/build_script.sh
+++ Scripts/build_script.sh
@@ -10,7 +10,6 @@ function deployToTestFlight
 curl http://testflightapp.com/api/builds.json -F file=@$build_location/$appname.ipa -F dsym=@$build_location/sym.root/Release-iphoneos/$appname.app.dSYM.zip -F api_token='439d831c7e49dbda0210fcc2821ac279_MTc2MzA5NTIwMTQtMDQtMDIgMDk6MjY6MzIuODYyNTg1' -F team_token='4eec8f2464f97aa31e333520338d7197_MzYyMjkzMjAxNC0wNC0wMiAwOTo0Mjo0Mi4xMzcxMTQ' -F notes='$appname uploaded via the testflight upload API' -F notify=True
 }
 function buildApp 
-
 {
 #!/bin/sh 
 
@@ -29,7 +28,9 @@ rm -f "$appname.app.dSYM.zip"
 zip -r "$appname.app.dSYM.zip" "$appname.app.dSYM"
   
 xcrun -sdk iphoneos PackageApplication -v "$build_location/sym.root/Release-iphoneos/$appname.app" -o "$build_location/$appname.ipa" --sign "$certificate"   
+
 deployToTestFlight
+
 }
 
 
