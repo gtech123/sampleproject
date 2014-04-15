b5e3a9c5e094c2ec0c61595c12490e8e80a4f457
hcl <gtech.hcl@gmail.com>
2014-04-15 15:08:24 +0530

    adding $appname

--- Scripts/build_script.sh
+++ Scripts/build_script.sh
@@ -8,7 +8,7 @@ build_location="/Users/admin/Documents/Builds/$appname"
 
 function deployToTestFlight    
 {
-curl http://testflightapp.com/api/builds.json -F file=@$build_location/sample.ipa -F dsym=@$build_location/sym.root/Release-iphoneos/sample.app.dSYM.zip -F api_token='439d831c7e49dbda0210fcc2821ac279_MTc2MzA5NTIwMTQtMDQtMDIgMDk6MjY6MzIuODYyNTg1' -F team_token='4eec8f2464f97aa31e333520338d7197_MzYyMjkzMjAxNC0wNC0wMiAwOTo0Mjo0Mi4xMzcxMTQ' -F notes='$appname uploaded via the testflight upload API' -F notify=True
+curl http://testflightapp.com/api/builds.json -F file=@$build_location/$appname.ipa -F dsym=@$build_location/sym.root/Release-iphoneos/$appname.app.dSYM.zip -F api_token='439d831c7e49dbda0210fcc2821ac279_MTc2MzA5NTIwMTQtMDQtMDIgMDk6MjY6MzIuODYyNTg1' -F team_token='4eec8f2464f97aa31e333520338d7197_MzYyMjkzMjAxNC0wNC0wMiAwOTo0Mjo0Mi4xMzcxMTQ' -F notes='$appname uploaded via the testflight upload API' -F notify=True
 }
 function buildApp 
 {
@@ -20,24 +20,16 @@ if [ ! -d "$build_location" ]; then
 mkdir -p "$build_location"/
 fi
 
-
-
-
-
-
-
-
-
 cd /Users/admin/Documents/sampleproject/
 xcodebuild -target "sampleproject" OBJROOT="$build_location/obj.root" SYMROOT="$build_location/sym.root"
 
 
 #zip dYSM file for distribution
 cd "$build_location/sym.root/Release-iphoneos/" || die "no such directory"
-rm -f "sample.app.dSYM.zip"
-zip -r "sample.app.dSYM.zip" "sample.app.dSYM"
+rm -f "$appname.app.dSYM.zip"
+zip -r "$appname.app.dSYM.zip" "$appname.app.dSYM"
   
-xcrun -sdk iphoneos PackageApplication -v "$build_location/sym.root/Release-iphoneos/sample.app" -o "$build_location/sample.ipa" --sign "$certificate"   
+xcrun -sdk iphoneos PackageApplication -v "$build_location/sym.root/Release-iphoneos/$appname.app" -o "$build_location/$appname.ipa" --sign "$certificate"   
 
 deployToTestFlight
 
